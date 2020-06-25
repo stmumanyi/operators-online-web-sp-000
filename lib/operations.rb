4cd300f49ca290a771fe1e0d9c1e5d2c223c5d87
Simba Mumanyi <simbamumanyi@gmail.com>
2020-06-25 18:53:06 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return TRUE
+  elsif speed < 40
+    return TRUE
+  else
+    return FALSE
+  end
 end
 
 
 
 def not_safe?(speed)
-	
-end
+  speed > 60 ? TRUE : FALSE || speed < 40 ? TRUE : FALSE
+  
+	end
 	
 
 
